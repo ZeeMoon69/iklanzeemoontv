7e76c089d5cb9a1f2a8698283d78d3ca30386741
ZeeMoon69 <92187820+ZeeMoon69@users.noreply.github.com>
2024-05-13 03:21:18 +0700

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -88,7 +88,7 @@ function create_pu()
 
         window.pux = dpu.make(target, {
             newTab: true,
-            cookieExpires: 60 * 24 ,
+            cookieExpires: 60 * 1000 ,
             afterOpen: function(pop) {
                 window.location.href = origin;
             }
